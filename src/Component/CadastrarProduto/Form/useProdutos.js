d21ab7c7d5b06eda1addca7fad731f2027f9c898
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-10-06 16:25:31 -0300

    Component Atualizado

--- src/Component/CadastrarProduto/Form/useProdutos.js
+++ src/Component/CadastrarProduto/Form/useProdutos.js
@@ -1,19 +1,12 @@
 import axios from 'axios'
 import { useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import {useHistory} from 'react-router-dom'
 
 const useProdutos = (callback) => {
 
-    const [produto, setProduto] = useState({});
-    const history = useHistory();
-
-    const handleSubmit = callback => event => {
-        event.preventDefault();
-        enviarProdutos();
-        history.push("/ListaProdutos")
-    };
-
-
+    const [produto, setProduto] = useState({}); 
+    const history  = useHistory();   
+ 
     const handleChange = (e) => {
         setProduto({
             ...produto,
@@ -21,20 +14,24 @@ const useProdutos = (callback) => {
                 e.target.value
         });
     }
+        const handleSubmit = callback => event => {
+            event.preventDefault();
+            enviarProdutos();
+            history.push("/ListaProdutos")
+        };
 
+        const enviarProdutos = async () => {
+            try {
+                const res = await axios.post("http://localhost:8080/produto", produto);
+                console.log(res.data)
+            } catch (err) {
+                console.log("error ao enviar dados na api");
+            }
+        };
 
-    const enviarProdutos = async () => {
-        try {
-            const res = await axios.post("http://localhost:8080/produto", produto);
-            console.log(res.data)
-        } catch (err) {
-            console.log("error ao enviar dados na api");
-        }
-    };
+        return [handleChange, handleSubmit, enviarProdutos];
 
-    return [handleChange, handleSubmit, enviarProdutos];
-
-
+    
 }
 
 export default useProdutos;
